1a8f63d4f10f28ccb4ffa674bb953069a4b7e482
Blissful <53982126+Blissful4992@users.noreply.github.com>
2021-05-24 18:55:00 +0200

    Update themes.lua

--- themes.lua
+++ themes.lua
@@ -4,10 +4,10 @@ local themes = {
         ["Window_Border"] = Color3.fromRGB(166, 94, 0),
         ["Close_Button"] = Color3.fromRGB(175, 100, 20),
 
-        ["Section_Title"] = Color3.fromRGB(210, 210, 210),
+        ["Section_Title"] = Color3.fromRGB(220, 220, 220),
         ["Section_Background"] = Color3.fromRGB(0, 0, 0),
 
-        ["Item_Name_Color"] = Color3.fromRGB(255, 255, 255),
+        ["Item_Name_Color"] = Color3.fromRGB(240, 240, 240),
 
         ["Button"] = Color3.fromRGB(166, 94, 0),
 
@@ -18,7 +18,7 @@ local themes = {
 
         ["Slider_Bar"] = Color3.fromRGB(166, 94, 0),
         ["Slider_Bob"] = Color3.fromRGB(255, 255, 255),
-        ["Slider_Value"] = Color3.fromRGB(160, 0, 255),
+        ["Slider_Value"] = Color3.fromRGB(255, 213, 153),
 
         ["Keybind_Border"] = Color3.fromRGB(166, 94, 0),
 
@@ -32,10 +32,10 @@ local themes = {
         ["Window_Border"] = Color3.fromRGB(0, 94, 0),
         ["Close_Button"] = Color3.fromRGB(40, 175, 100),
 
-        ["Section_Title"] = Color3.fromRGB(191, 255, 189),
+        ["Section_Title"] = Color3.fromRGB(220, 220, 220),
         ["Section_Background"] = Color3.fromRGB(0, 0, 20),
 
-        ["Item_Name_Color"] = Color3.fromRGB(220, 220, 220),
+        ["Item_Name_Color"] = Color3.fromRGB(240, 240, 240),
 
         ["Button"] = Color3.fromRGB(0, 94, 0),
 
@@ -46,12 +46,12 @@ local themes = {
 
         ["Slider_Bar"] = Color3.fromRGB(0, 94, 0),
         ["Slider_Bob"] = Color3.fromRGB(220, 220, 220),
-        ["Slider_Value"] = Color3.fromRGB(60, 190, 115),
+        ["Slider_Value"] = Color3.fromRGB(191, 255, 189),
 
         ["Keybind_Border"] = Color3.fromRGB(0, 94, 0),
 
         ["Dropdown_Border"] = Color3.fromRGB(0, 94, 0),
-        ["Dropdown_Arrow"] = Color3.fromRGB(60, 190, 115),
+        ["Dropdown_Arrow"] = Color3.fromRGB(255, 255, 255),
         ["Dropdown_Main_Option"] = Color3.fromRGB(191, 255, 189),
         ["Dropdown_Options"] = Color3.fromRGB(220, 220, 220),
     },
@@ -60,10 +60,10 @@ local themes = {
         ["Window_Border"] = Color3.fromRGB(67, 97, 238),
         ["Close_Button"] = Color3.fromRGB(67, 97, 238),
 
-        ["Section_Title"] = Color3.fromRGB(122, 206, 255),
+        ["Section_Title"] = Color3.fromRGB(220, 220, 220),
         ["Section_Background"] = Color3.fromRGB(0, 0, 20),
 
-        ["Item_Name_Color"] = Color3.fromRGB(220, 220, 220),
+        ["Item_Name_Color"] = Color3.fromRGB(240, 240, 240),
 
         ["Button"] = Color3.fromRGB(67, 97, 238),
 
@@ -74,13 +74,13 @@ local themes = {
 
         ["Slider_Bar"] = Color3.fromRGB(67, 97, 238),
         ["Slider_Bob"] = Color3.fromRGB(220, 220, 220),
-        ["Slider_Value"] = Color3.fromRGB(176, 254, 255),
+        ["Slider_Value"] = Color3.fromRGB(175, 240, 255),
 
         ["Keybind_Border"] = Color3.fromRGB(67, 97, 238),
 
         ["Dropdown_Border"] = Color3.fromRGB(67, 97, 238),
         ["Dropdown_Arrow"] = Color3.fromRGB(67, 97, 238),
-        ["Dropdown_Main_Option"] = Color3.fromRGB(176, 254, 255),
+        ["Dropdown_Main_Option"] = Color3.fromRGB(175, 240, 255),
         ["Dropdown_Options"] = Color3.fromRGB(220, 220, 220),
     },
 
@@ -88,7 +88,7 @@ local themes = {
         ["Window_Border"] = Color3.fromRGB(140, 140, 140),
         ["Close_Button"] = Color3.fromRGB(210, 210, 210),
 
-        ["Section_Title"] = Color3.fromRGB(240, 240, 240),
+        ["Section_Title"] = Color3.fromRGB(220, 220, 220),
         ["Section_Background"] = Color3.fromRGB(0, 0, 20),
 
         ["Item_Name_Color"] = Color3.fromRGB(240, 240, 240),
@@ -109,7 +109,7 @@ local themes = {
         ["Dropdown_Border"] = Color3.fromRGB(140, 140, 140),
         ["Dropdown_Arrow"] = Color3.fromRGB(140, 140, 140),
         ["Dropdown_Main_Option"] = Color3.fromRGB(240, 240, 240),
-        ["Dropdown_Options"] = Color3.fromRGB(222, 226, 230),
+        ["Dropdown_Options"] = Color3.fromRGB(220, 220, 220),
     },
 
     ["Midnight"] = {
@@ -130,14 +130,14 @@ local themes = {
 
         ["Slider_Bar"] = Color3.fromRGB(75, 0, 105),
         ["Slider_Bob"] = Color3.fromRGB(220, 220, 220),
-        ["Slider_Value"] = Color3.fromRGB(220, 220, 220),
+        ["Slider_Value"] = Color3.fromRGB(240, 240, 240),
 
         ["Keybind_Border"] = Color3.fromRGB(69, 4, 122),
 
         ["Dropdown_Border"] = Color3.fromRGB(69, 4, 122),
-        ["Dropdown_Arrow"] = Color3.fromRGB(69, 4, 122),
+        ["Dropdown_Arrow"] = Color3.fromRGB(255, 255, 255),
         ["Dropdown_Main_Option"] = Color3.fromRGB(220, 220, 220),
-        ["Dropdown_Options"] = Color3.fromRGB(222, 226, 230),
+        ["Dropdown_Options"] = Color3.fromRGB(220, 220, 220),
     }
 }
 
