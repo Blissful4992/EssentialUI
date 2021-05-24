728cadb1a6f3ebd023f820a966fb3b938d39b8ac
Blissful <53982126+Blissful4992@users.noreply.github.com>
2021-05-24 14:20:32 +0200

    Update themes.lua

--- themes.lua
+++ themes.lua
@@ -1,143 +1,144 @@
-local themes = {}
+local themes = {
 
-themes["Fire"] = {
-    ["Window_Border"] = Color3.fromRGB(166, 94, 0),
-    ["Close_Button"] = Color3.fromRGB(175, 100, 20),
+    ["Fire"] = {
+        ["Window_Border"] = Color3.fromRGB(166, 94, 0),
+        ["Close_Button"] = Color3.fromRGB(175, 100, 20),
 
-    ["Section_Title"] = Color3.fromRGB(210, 210, 210),
-    ["Section_Background"] = Color3.fromRGB(0, 0, 0),
+        ["Section_Title"] = Color3.fromRGB(210, 210, 210),
+        ["Section_Background"] = Color3.fromRGB(0, 0, 0),
 
-    ["Item_Name_Color"] = Color3.fromRGB(255, 255, 255),
+        ["Item_Name_Color"] = Color3.fromRGB(255, 255, 255),
 
-    ["Button"] = Color3.fromRGB(166, 94, 0),
+        ["Button"] = Color3.fromRGB(166, 94, 0),
 
-    ["Toggle"] = Color3.fromRGB(166, 94, 0),
+        ["Toggle"] = Color3.fromRGB(166, 94, 0),
 
-    ["Color_Picker"] = Color3.fromRGB(166, 94, 0),
-    ["Color_Picker_Selector_Frame"] = Color3.fromRGB(166, 94, 0),
+        ["Color_Picker"] = Color3.fromRGB(166, 94, 0),
+        ["Color_Picker_Selector_Frame"] = Color3.fromRGB(166, 94, 0),
 
-    ["Slider_Bar"] = Color3.fromRGB(166, 94, 0),
-    ["Slider_Bob"] = Color3.fromRGB(255, 255, 255),
-    ["Slider_Value"] = Color3.fromRGB(160, 0, 255),
+        ["Slider_Bar"] = Color3.fromRGB(166, 94, 0),
+        ["Slider_Bob"] = Color3.fromRGB(255, 255, 255),
+        ["Slider_Value"] = Color3.fromRGB(160, 0, 255),
 
-    ["Keybind_Border"] = Color3.fromRGB(166, 94, 0),
+        ["Keybind_Border"] = Color3.fromRGB(166, 94, 0),
 
-    ["Dropdown_Border"] = Color3.fromRGB(166, 94, 0),
-    ["Dropdown_Arrow"] = Color3.fromRGB(255, 255, 255),
-    ["Dropdown_Main_Option"] = Color3.fromRGB(255, 213, 153),
-    ["Dropdown_Options"] = Color3.fromRGB(255, 255, 255),
-}
+        ["Dropdown_Border"] = Color3.fromRGB(166, 94, 0),
+        ["Dropdown_Arrow"] = Color3.fromRGB(255, 255, 255),
+        ["Dropdown_Main_Option"] = Color3.fromRGB(255, 213, 153),
+        ["Dropdown_Options"] = Color3.fromRGB(255, 255, 255),
+    },
 
-themes["Forest"] = {
-    ["Window_Border"] = Color3.fromRGB(0, 94, 0),
-    ["Close_Button"] = Color3.fromRGB(40, 175, 100),
-    
-    ["Section_Title"] = Color3.fromRGB(191, 255, 189),
-    ["Section_Background"] = Color3.fromRGB(0, 0, 20),
+    ["Forest"] = {
+        ["Window_Border"] = Color3.fromRGB(0, 94, 0),
+        ["Close_Button"] = Color3.fromRGB(40, 175, 100),
 
-    ["Item_Name_Color"] = Color3.fromRGB(220, 220, 220),
+        ["Section_Title"] = Color3.fromRGB(191, 255, 189),
+        ["Section_Background"] = Color3.fromRGB(0, 0, 20),
 
-    ["Button"] = Color3.fromRGB(0, 94, 0),
+        ["Item_Name_Color"] = Color3.fromRGB(220, 220, 220),
 
-    ["Toggle"] = Color3.fromRGB(0, 94, 0),
+        ["Button"] = Color3.fromRGB(0, 94, 0),
 
-    ["Color_Picker"] = Color3.fromRGB(0, 94, 0),
-    ["Color_Picker_Selector_Frame"] = Color3.fromRGB(0, 94, 0),
+        ["Toggle"] = Color3.fromRGB(0, 94, 0),
 
-    ["Slider_Bar"] = Color3.fromRGB(0, 94, 0),
-    ["Slider_Bob"] = Color3.fromRGB(220, 220, 220),
-    ["Slider_Value"] = Color3.fromRGB(60, 190, 115),
+        ["Color_Picker"] = Color3.fromRGB(0, 94, 0),
+        ["Color_Picker_Selector_Frame"] = Color3.fromRGB(0, 94, 0),
 
-    ["Keybind_Border"] = Color3.fromRGB(0, 94, 0),
+        ["Slider_Bar"] = Color3.fromRGB(0, 94, 0),
+        ["Slider_Bob"] = Color3.fromRGB(220, 220, 220),
+        ["Slider_Value"] = Color3.fromRGB(60, 190, 115),
 
-    ["Dropdown_Border"] = Color3.fromRGB(0, 94, 0),
-    ["Dropdown_Arrow"] = Color3.fromRGB(60, 190, 115),
-    ["Dropdown_Main_Option"] = Color3.fromRGB(191, 255, 189),
-    ["Dropdown_Options"] = Color3.fromRGB(220, 220, 220),
-}
+        ["Keybind_Border"] = Color3.fromRGB(0, 94, 0),
 
-themes["Ocean"] = {
-    ["Window_Border"] = Color3.fromRGB(67, 97, 238),
-    ["Close_Button"] = Color3.fromRGB(67, 97, 238),
-    
-    ["Section_Title"] = Color3.fromRGB(122, 206, 255),
-    ["Section_Background"] = Color3.fromRGB(0, 0, 20),
+        ["Dropdown_Border"] = Color3.fromRGB(0, 94, 0),
+        ["Dropdown_Arrow"] = Color3.fromRGB(60, 190, 115),
+        ["Dropdown_Main_Option"] = Color3.fromRGB(191, 255, 189),
+        ["Dropdown_Options"] = Color3.fromRGB(220, 220, 220),
+    },
 
-    ["Item_Name_Color"] = Color3.fromRGB(220, 220, 220),
+    ["Ocean"] = {
+        ["Window_Border"] = Color3.fromRGB(67, 97, 238),
+        ["Close_Button"] = Color3.fromRGB(67, 97, 238),
 
-    ["Button"] = Color3.fromRGB(67, 97, 238),
+        ["Section_Title"] = Color3.fromRGB(122, 206, 255),
+        ["Section_Background"] = Color3.fromRGB(0, 0, 20),
 
-    ["Toggle"] = Color3.fromRGB(67, 97, 238),
+        ["Item_Name_Color"] = Color3.fromRGB(220, 220, 220),
 
-    ["Color_Picker"] = Color3.fromRGB(67, 97, 238),
-    ["Color_Picker_Selector_Frame"] = Color3.fromRGB(67, 97, 238),
+        ["Button"] = Color3.fromRGB(67, 97, 238),
 
-    ["Slider_Bar"] = Color3.fromRGB(67, 97, 238),
-    ["Slider_Bob"] = Color3.fromRGB(220, 220, 220),
-    ["Slider_Value"] = Color3.fromRGB(176, 254, 255),
+        ["Toggle"] = Color3.fromRGB(67, 97, 238),
 
-    ["Keybind_Border"] = Color3.fromRGB(67, 97, 238),
+        ["Color_Picker"] = Color3.fromRGB(67, 97, 238),
+        ["Color_Picker_Selector_Frame"] = Color3.fromRGB(67, 97, 238),
 
-    ["Dropdown_Border"] = Color3.fromRGB(67, 97, 238),
-    ["Dropdown_Arrow"] = Color3.fromRGB(67, 97, 238),
-    ["Dropdown_Main_Option"] = Color3.fromRGB(176, 254, 255),
-    ["Dropdown_Options"] = Color3.fromRGB(220, 220, 220),
-}
+        ["Slider_Bar"] = Color3.fromRGB(67, 97, 238),
+        ["Slider_Bob"] = Color3.fromRGB(220, 220, 220),
+        ["Slider_Value"] = Color3.fromRGB(176, 254, 255),
 
-themes["Grayscale"] = {
-    ["Window_Border"] = Color3.fromRGB(140, 140, 140),
-    ["Close_Button"] = Color3.fromRGB(210, 210, 210),
-    
-    ["Section_Title"] = Color3.fromRGB(240, 240, 240),
-    ["Section_Background"] = Color3.fromRGB(0, 0, 20),
+        ["Keybind_Border"] = Color3.fromRGB(67, 97, 238),
 
-    ["Item_Name_Color"] = Color3.fromRGB(240, 240, 240),
+        ["Dropdown_Border"] = Color3.fromRGB(67, 97, 238),
+        ["Dropdown_Arrow"] = Color3.fromRGB(67, 97, 238),
+        ["Dropdown_Main_Option"] = Color3.fromRGB(176, 254, 255),
+        ["Dropdown_Options"] = Color3.fromRGB(220, 220, 220),
+    },
 
-    ["Button"] = Color3.fromRGB(140, 140, 140),
+    ["Grayscale"] = {
+        ["Window_Border"] = Color3.fromRGB(140, 140, 140),
+        ["Close_Button"] = Color3.fromRGB(210, 210, 210),
 
-    ["Toggle"] = Color3.fromRGB(140, 140, 140),
+        ["Section_Title"] = Color3.fromRGB(240, 240, 240),
+        ["Section_Background"] = Color3.fromRGB(0, 0, 20),
 
-    ["Color_Picker"] = Color3.fromRGB(140, 140, 140),
-    ["Color_Picker_Selector_Frame"] = Color3.fromRGB(140, 140, 140),
+        ["Item_Name_Color"] = Color3.fromRGB(240, 240, 240),
 
-    ["Slider_Bar"] = Color3.fromRGB(140, 140, 140),
-    ["Slider_Bob"] = Color3.fromRGB(240, 240, 240),
-    ["Slider_Value"] = Color3.fromRGB(240, 240, 240),
+        ["Button"] = Color3.fromRGB(140, 140, 140),
 
-    ["Keybind_Border"] = Color3.fromRGB(140, 140, 140),
+        ["Toggle"] = Color3.fromRGB(140, 140, 140),
 
-    ["Dropdown_Border"] = Color3.fromRGB(140, 140, 140),
-    ["Dropdown_Arrow"] = Color3.fromRGB(140, 140, 140),
-    ["Dropdown_Main_Option"] = Color3.fromRGB(240, 240, 240),
-    ["Dropdown_Options"] = Color3.fromRGB(222, 226, 230),
-}
+        ["Color_Picker"] = Color3.fromRGB(140, 140, 140),
+        ["Color_Picker_Selector_Frame"] = Color3.fromRGB(140, 140, 140),
 
-themes["Midnight"] = {
-    ["Window_Border"] = Color3.fromRGB(69, 4, 122),
-    ["Close_Button"] = Color3.fromRGB(210, 210, 210),
-    
-    ["Section_Title"] = Color3.fromRGB(220, 220, 220),
-    ["Section_Background"] = Color3.fromRGB(0, 0, 20),
+        ["Slider_Bar"] = Color3.fromRGB(140, 140, 140),
+        ["Slider_Bob"] = Color3.fromRGB(240, 240, 240),
+        ["Slider_Value"] = Color3.fromRGB(240, 240, 240),
 
-    ["Item_Name_Color"] = Color3.fromRGB(240, 240, 240),
+        ["Keybind_Border"] = Color3.fromRGB(140, 140, 140),
 
-    ["Button"] = Color3.fromRGB(69, 4, 122),
+        ["Dropdown_Border"] = Color3.fromRGB(140, 140, 140),
+        ["Dropdown_Arrow"] = Color3.fromRGB(140, 140, 140),
+        ["Dropdown_Main_Option"] = Color3.fromRGB(240, 240, 240),
+        ["Dropdown_Options"] = Color3.fromRGB(222, 226, 230),
+    }
 
-    ["Toggle"] = Color3.fromRGB(69, 4, 122),
+    ["Midnight"] = {
+        ["Window_Border"] = Color3.fromRGB(69, 4, 122),
+        ["Close_Button"] = Color3.fromRGB(210, 210, 210),
 
-    ["Color_Picker"] = Color3.fromRGB(69, 4, 122),
-    ["Color_Picker_Selector_Frame"] = Color3.fromRGB(69, 4, 122),
+        ["Section_Title"] = Color3.fromRGB(220, 220, 220),
+        ["Section_Background"] = Color3.fromRGB(0, 0, 20),
 
-    ["Slider_Bar"] = Color3.fromRGB(75, 0, 105),
-    ["Slider_Bob"] = Color3.fromRGB(220, 220, 220),
-    ["Slider_Value"] = Color3.fromRGB(220, 220, 220),
+        ["Item_Name_Color"] = Color3.fromRGB(240, 240, 240),
 
-    ["Keybind_Border"] = Color3.fromRGB(69, 4, 122),
+        ["Button"] = Color3.fromRGB(69, 4, 122),
 
-    ["Dropdown_Border"] = Color3.fromRGB(69, 4, 122),
-    ["Dropdown_Arrow"] = Color3.fromRGB(69, 4, 122),
-    ["Dropdown_Main_Option"] = Color3.fromRGB(220, 220, 220),
-    ["Dropdown_Options"] = Color3.fromRGB(222, 226, 230),
+        ["Toggle"] = Color3.fromRGB(69, 4, 122),
+
+        ["Color_Picker"] = Color3.fromRGB(69, 4, 122),
+        ["Color_Picker_Selector_Frame"] = Color3.fromRGB(69, 4, 122),
+
+        ["Slider_Bar"] = Color3.fromRGB(75, 0, 105),
+        ["Slider_Bob"] = Color3.fromRGB(220, 220, 220),
+        ["Slider_Value"] = Color3.fromRGB(220, 220, 220),
+
+        ["Keybind_Border"] = Color3.fromRGB(69, 4, 122),
+
+        ["Dropdown_Border"] = Color3.fromRGB(69, 4, 122),
+        ["Dropdown_Arrow"] = Color3.fromRGB(69, 4, 122),
+        ["Dropdown_Main_Option"] = Color3.fromRGB(220, 220, 220),
+        ["Dropdown_Options"] = Color3.fromRGB(222, 226, 230),
+    }
 }
 
 return themes
